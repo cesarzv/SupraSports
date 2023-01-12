a7f8a9ee3ecb61ee81796fa83487e8fdafa9a653
cesarzv <cesar-x26@hotmail.com>
2023-01-12 15:46:31 -0500

    wdrrr

--- server/src/app.js
+++ server/src/app.js
@@ -27,7 +27,7 @@ server.use((req, res, next) => {
 });
 server.use(
   cors({
-    origin: 'https://supra-sports.vercel.app',
+    origin: 'https://ecommerce-deploy-production.up.railway.app/',
     methods: ['GET', 'PUT', 'POST', 'DELETE'],
     allowedHeaders: ['Authorization', 'Content-Type'],
     maxAge: 86400,
